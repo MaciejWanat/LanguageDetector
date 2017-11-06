99beaf2284a56f3f60a9dde6ce5ff271e32a1692
Maciej Wanat <maciejwanat@10g.pl>
2017-11-06 20:12:15 +0100

    Extended ngrams

--- lib/ngram.rb
+++ lib/ngram.rb
@@ -1,4 +1,3 @@
 def ngrams(n, string)
 	string.downcase.tr('²→³−±°@%&↑\{}"[]α·', '').gsub(/[[:punct:]]/, ' ').gsub(/[0-9]/, ' ').squeeze(' ').gsub("\n",'').gsub(Regexp.union(" "), "_").split("").each_cons(n).to_a
-	#²→³−±°@%&↑\{\}\"\[\]α·
 end
